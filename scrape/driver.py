340aa1892cc8a88500dcb6277ba95ee53b833833
Kostia <kostia3507@gmail.com>
2024-01-10 18:01:12 +0200

    changed return annotation for new methond in Driver class

--- scrape/driver.py
+++ scrape/driver.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 
 class ChromeDriver:
-    def __new__(cls) -> ChromeDriver:
+    def __new__(cls) -> None:
         if not hasattr(cls, "instance"):
             cls.instance = super(ChromeDriver, cls).__new__(cls)
         return cls.instance
